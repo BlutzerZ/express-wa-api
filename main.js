d7e0966f3300ef1f3a7d2432fa78981640436424
BlutzerZ <wafiqnurudin@gmail.com>
2024-10-25 14:05:28 +0700

    allow all cors

--- main.js
+++ main.js
@@ -9,9 +9,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-    origin: 'http://127.0.0.1:5500'
-  }));
+app.use(cors());
 
 let sock; // Socket instance for WhatsApp connection
 let isLoggedIn = false; // Status login WhatsApp
