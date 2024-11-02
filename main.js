ea978557eb20212271929583a882b91543e48924
BlutzerZ <wafiqnurudin@gmail.com>
2024-11-02 18:15:40 +0700

    update cors option

--- main.js
+++ main.js
@@ -9,7 +9,14 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+
+const corsOptions = {
+    origin: '*', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+};
+
+app.use(cors(corsOptions));
 
 let sock; // Socket instance for WhatsApp connection
 let isLoggedIn = false; // Status login WhatsApp
